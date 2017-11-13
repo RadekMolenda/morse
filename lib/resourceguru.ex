ad4affc1ccbf93836bba8471fc37a717eab48bb0
Radek Molenda <radek.molenda@gmail.com>
2017-11-13 20:35:19 +0100

    flatten the list recursively

--- lib/resourceguru.ex
+++ lib/resourceguru.ex
@@ -1,18 +1,24 @@
 defmodule Resourceguru do
   @moduledoc """
-  Documentation for Resourceguru.
+  module that flattens the array
   """
 
   @doc """
-  Hello world.
+  flattens the list.
 
   ## Examples
 
-      iex> Resourceguru.hello
-      :world
+      iex> Resourceguru.flatten([])
+      []
+      iex> Resourceguru.flatten([1])
+      [1]
+      iex> Resourceguru.flatten([1, [2, [3]], 4])
+      [1, 2, 3, 4]
 
   """
-  def hello do
-    :world
-  end
+  def flatten(list), do: flatten(list, []) |> Enum.reverse
+  def flatten([[] | t], acc), do: flatten(t, acc)
+  def flatten([h | t], acc) when is_list(h), do: flatten(t, flatten(h, acc))
+  def flatten([h | t], acc), do: flatten(t, [h | acc])
+  def flatten([], acc), do: acc
 end
--- test/resourceguru_test.exs
+++ test/resourceguru_test.exs
@@ -2,7 +2,23 @@ defmodule ResourceguruTest do
   use ExUnit.Case
   doctest Resourceguru
 
-  test "greets the world" do
-    assert Resourceguru.hello() == :world
+  test "flattens the empty list" do
+    assert Resourceguru.flatten([]) == []
+  end
+
+  test "flattens the nested empty lists" do
+    assert Resourceguru.flatten([[[]]]) == []
+  end
+
+  test "flattens the lists with one element" do
+    assert Resourceguru.flatten([1]) == [1]
+  end
+
+  test "flattens the lists with one nested element" do
+    assert Resourceguru.flatten([[1]]) == [1]
+  end
+
+  test "flattens the array of integers" do
+    assert Resourceguru.flatten([1, [2, [3]], 4]) == [1, 2, 3, 4]
   end
 end
